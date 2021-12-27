09228420d373f5319776f1f011fe96e090c3dfff
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-12-27 14:27:35 +0100

    static comp

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { Input, Segment } from "semantic-ui-react";
 import "../../styles/header.scss";
 
-const Header = () => <div>Header</div>;
+const Header = () => (
+    <header className="header">
+        <img className="header__logo" src="./img/GitHub-logo.png" alt="logo" />
+        <Segment>
+            <form>
+                <Input
+                    fluid
+                    icon="search"
+                    iconPosition="left"
+                    placeholder="Chercher un repo"
+                />
+            </form>
+        </Segment>
+    </header>
+);
 
 export default Header;
--- client/src/components/Repos/index.js
+++ client/src/components/Repos/index.js
@@ -1,6 +1,52 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 import "../../styles/repos.scss";
 
-const Repos = () => <div>Repos</div>;
+const Repos = () => (
+    <Card.Group itemsPerRow={3}>
+        <Card
+            image="https://avatars3.githubusercontent.com/u/698437?v=4"
+            header="Elliot Baker"
+            meta="Friend"
+            description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
+        />
+        <Card
+            image="https://avatars3.githubusercontent.com/u/698437?v=4"
+            header="Elliot Baker"
+            meta="Friend"
+            description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
+        />
+        <Card
+            image="https://avatars3.githubusercontent.com/u/698437?v=4"
+            header="Elliot Baker"
+            meta="Friend"
+            description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
+        />
+        <Card
+            image="https://avatars3.githubusercontent.com/u/698437?v=4"
+            header="Elliot Baker"
+            meta="Friend"
+            description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
+        />
+        <Card
+            image="https://avatars3.githubusercontent.com/u/698437?v=4"
+            header="Elliot Baker"
+            meta="Friend"
+            description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
+        />
+        <Card
+            image="https://avatars3.githubusercontent.com/u/698437?v=4"
+            header="Elliot Baker"
+            meta="Friend"
+            description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
+        />
+        <Card
+            image="https://avatars3.githubusercontent.com/u/698437?v=4"
+            header="Elliot Baker"
+            meta="Friend"
+            description="Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat."
+        />
+    </Card.Group>
+);
 
 export default Repos;
