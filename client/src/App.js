2bc12a86f16a370ce01b435639399b381179a801
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-12-27 17:34:51 +0100

    adding BASE_URL + usestate for repos search

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,73 @@
 // == Import npm
 // == Import
+import axios from "axios";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import React, { useState } from "react";
 import { Message } from "semantic-ui-react";
 import Header from "./components/Header";
 import Repos from "./components/Repos";
-import data from "./data/repos.js";
 import "./styles/App.scss";
 
-const App = () => (
-    <div className="app">
-        <Header
-            inputValue="Coucou"
-            onInputChange={(textSaisi) => {
-                console.log("change with ", textSaisi);
-            }}
-            onFormSubmit={() => {
-                console.log("submit");
-            }}
-        />
-        <Message content="je suis un message" />
-        <Repos list={data.items} />
-    </div>
-);
+const BASE_URL = "https://api.github.com/search/repositories?q=";
+
+const App = () => {
+    const [message, setMessage] = useState(
+        "Aucun résultat pour le moment, faites une recherche "
+    );
+
+    const [loading, setLoading] = useState(false);
+
+    // state for stock up my list
+    const [repos, setRepos] = useState([]);
+
+    // i want to have in sotkc my input, and i want the "method" to change him.
+    const [inputText, setInputText] = useState("");
+
+    // i want to change this input text
+    const handleInputChange = (textSaisi) => {
+        setInputText(textSaisi);
+    };
+
+    // when i Submit
+    const handleFormSubmit = () => {
+        setLoading(true);
+        // made my request
+        axios({
+            method: "get",
+            url: `${BASE_URL}${inputText}`,
+        })
+            // if it's working, had repos on state
+            .then((res) => {
+                // axios send me the server response in res.data
+                // console.log(res.data)
+                // i put the repos in the state
+                setRepos(res.data.items);
+                const newMessage = `La recherche a donné ${res.data.total_count} repos`;
+                setMessage(newMessage);
+            })
+            // if not, send a error message
+            .catch((err) => {
+                console.trace(err);
+            })
+
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+    return (
+        <div className="app">
+            <Header
+                loading={loading}
+                inputValue={inputText}
+                onInputChange={handleInputChange}
+                onFormSubmit={handleFormSubmit}
+            />
+            <Message content={message} />
+            <Repos list={repos} />
+        </div>
+    );
+};
 
 // == Export
 export default App;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,31 +1,45 @@
+import { Icon } from "@iconify/react";
 import React from "react";
 import "../../styles/header.scss";
 
-const Header = ({ inputValue, onInputChange, onFormSubmit }) => (
-    <header className="header">
-        <img className="header__logo" src="./img/GitHub-logo.png" alt="logo" />
-        <div>
-            <form
-                class="d-flex"
-                onSubmit={(evt) => {
-                    evt.preventDefault();
-                    onFormSubmit();
-                }}
-            >
-                <input
-                    class="form-control me-2"
-                    type="search"
-                    placeholder="Chercher un repo"
-                    aria-label="Search"
-                    value={inputValue}
-                    onChange={(evt) => {
-                        const text = evt.target.value;
-                        onInputChange(text);
+const Header = ({ inputValue, onInputChange, onFormSubmit, loading }) => {
+    return (
+        <header className="header">
+            <img
+                className="header__logo"
+                src="./img/GitHub-logo.png"
+                alt="logo"
+            />
+            <div class="input-group w-100">
+                <form
+                    class="d-flex w-100"
+                    onSubmit={(evt) => {
+                        evt.preventDefault();
+                        onFormSubmit();
                     }}
-                ></input>
-            </form>
-        </div>
-    </header>
-);
+                >
+                    <span class="input-group-text" id="basic-addon1">
+                        {loading ? (
+                            <Icon icon="eos-icons:loading" />
+                        ) : (
+                            <Icon icon="ant-design:search-outlined" />
+                        )}
+                    </span>
+                    <input
+                        class="form-control "
+                        type="text"
+                        placeholder="Chercher un repo"
+                        aria-label="Search"
+                        value={inputValue}
+                        onChange={(evt) => {
+                            const text = evt.target.value;
+                            onInputChange(text);
+                        }}
+                    ></input>
+                </form>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
